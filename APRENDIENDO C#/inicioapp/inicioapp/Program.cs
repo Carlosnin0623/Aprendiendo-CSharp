7d37f7236749eba73d89f7142e597403c32ede99
Carlos Alberto Nin Queliz <carlosnin648@gmail.com>
2025-01-29 14:28:53 -0400

    Aprendiendo C#

---
+++ APRENDIENDO C#/inicioapp/inicioapp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inicioapp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Console.WriteLine nos permite mosrar por consola un mensaje */
+            Console.WriteLine("Hola mundo");
+
+            /* Console.ReadKey(): Permite que la consola no se cierre hasta que el usuario presione una tecla */
+           /* Console.ReadKey(); */
+
+            /* Console.ReadLine(): Lee la siguiene linea de caracteres */
+
+            Console.ReadLine();
+
+        }
+    }
+}
