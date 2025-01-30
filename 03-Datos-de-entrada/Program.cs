95d62ce2c7cafb860bd987b3513e61ad853420f1
Carlosnin0623 <carlosnin648@gmail.com>
2025-01-29 22:17:47 -0400

    Datos de entrada

---
+++ 03-Datos-de-entrada/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_Datos_de_entrada
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            int edad;
+            double estatura;
+
+            Console.WriteLine("Ingrese su nombre: ");
+
+            // ReadLine le pide datos al usuario para almacenarlo en una variable //
+            nombre = Console.ReadLine();
+
+            Console.WriteLine("Ingrese su edad: ");
+
+            edad = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Ingrese su altura: ");
+
+            estatura = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Su nombre es: {0} su su edad es: {1} y su estatura es: {2}", nombre, edad, estatura);
+
+            Console.ReadKey();
+        }
+    }
+}
