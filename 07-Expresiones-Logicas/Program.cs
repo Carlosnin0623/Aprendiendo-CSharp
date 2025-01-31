38f13e4094d955a40cadc9a83218d7ebf493c282
Carlos Alberto Nin Queliz <carlosnin648@gmail.com>
2025-01-31 12:09:07 -0400

    07-Expresiones-logicas

---
+++ 07-Expresiones-Logicas/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_Expresiones_Logicas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 
+               Expresiones logicas
+                   
+                   Operador && significado Y:
+                   Se evalua como verdadera unicamente cuando ambas expresiones son verdaderas
+                   
+                      P  Q   resultado
+                      V  V      V
+                      F  V      F
+                      V  F      F
+                      F  F      F
+             */
+
+            short tanque_gasolina = 50;
+            short recorrido = 400;
+
+            if(tanque_gasolina <= 40 && recorrido >= 400)
+            {
+                Console.WriteLine("Por favor llena el tanque");
+            }
+            else if (tanque_gasolina >= 50 && recorrido >= 400)
+            {
+                Console.WriteLine("Disfruta el viaje");
+            }
+
+            bool lluvia = false;
+            bool sol = false;
+
+            /* 
+             Expresiones logicas
+
+                 Operador || significado O:
+                 Se evalua como verdadera cuando almenos 1 sea verdadera
+
+                    P  Q   resultado
+                    V  V      V
+                    F  V      V
+                    V  F      V
+                    F  F      F
+           */
+
+            if (lluvia == true || sol == true)
+            {
+                Console.WriteLine("Debes usar sombrilla");
+            }
+            else
+            {
+                Console.WriteLine("Puedes salir tranquilamente");
+            }
+
+            /* Operador de negación 
+             
+               Simbolo       Significado      Condición
+                  !           Negación        Esto quiere decir que si el operando del lado derecho
+                                              es true, el operador regresara false, y si el operando
+                                              del lado derecho es false, el operador regresara true
+
+
+                p            Negacion
+               True            False
+               False           True
+             
+             */
+
+              bool dia = false;
+
+
+            if(!dia == false)
+            {
+                Console.WriteLine("Es de dia");
+            }
+            else
+            {
+                Console.WriteLine("Es de noche");
+            }
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 08-Estructura-de-control-Swich/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("08-Estructura-de-control-Swich")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("08-Estructura-de-control-Swich")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("20b9af3f-ab83-4c57-98e3-a369fdc40da5")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
