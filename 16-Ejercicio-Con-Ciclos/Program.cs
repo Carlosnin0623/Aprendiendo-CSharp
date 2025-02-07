1562fdd7b19027d53081e18c81caadeeec465711
Carlos Alberto Nin Queliz <carlosnin648@gmail.com>
2025-02-07 13:49:02 -0400

    Ejercicio Bucles

--- 16-Ejercicio-Con-Ciclos/Program.cs
+++ 16-Ejercicio-Con-Ciclos/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace _16_Ejercicio_Con_Ciclos
@@ -10,87 +11,16 @@ namespace _16_Ejercicio_Con_Ciclos
     {
         static void Main(string[] args)
         {
-            short num1, num2;
+            byte num;
+            Console.WriteLine("Introduce el número: ");
+            num = Convert.ToByte(Console.ReadLine());
 
-            int resultado;
-
-            byte opcion;
-            try
+            num-=1;
+            
+            for(byte valor = num; valor >= 1; valor--)
             {
-                do
-                {
-                    Console.WriteLine("Bienvenido a mi calculadora \n");
-                    Console.WriteLine("¿Que deseas hacer? \n");
-                    Console.WriteLine("(1) Sumar");
-                    Console.WriteLine("(2) Restar");
-                    Console.WriteLine("(3) Multiplicar");
-                    Console.WriteLine("(4) dividir");
-                    Console.WriteLine("(5) Salir");
-                    opcion = Convert.ToByte(Console.ReadLine());
-
-                    switch (opcion)
-                    {
-                        case 1:
-                            Console.WriteLine("Digita el primer número: ");
-                            num1 = Convert.ToInt16(Console.ReadLine());
-
-                            Console.WriteLine("Digita el segundo número: ");
-                            num2 = Convert.ToInt16(Console.ReadLine());
-
-                            resultado = num1 + num2;
-
-                            Console.WriteLine("El resultado es: {0}", resultado);
-
-                            break;
-
-                        case 2:
-                            Console.WriteLine("Digita el primer número: ");
-                            num1 = Convert.ToInt16(Console.ReadLine());
-
-                            Console.WriteLine("Digita el segundo número: ");
-                            num2 = Convert.ToInt16(Console.ReadLine());
-
-                            resultado = num1 - num2;
-
-                            Console.WriteLine("El resultado es: {0}", resultado);
-
-                            break;
-
-                        case 3:
-                            Console.WriteLine("Digita el primer número: ");
-                            num1 = Convert.ToInt16(Console.ReadLine());
-
-                            Console.WriteLine("Digita el segundo número: ");
-                            num2 = Convert.ToInt16(Console.ReadLine());
-
-                            resultado = num1 * num2;
-
-                            Console.WriteLine("El resultado es: {0}", resultado);
-
-                            break;
-
-                        case 4:
-                            Console.WriteLine("Digita el primer número: ");
-                            num1 = Convert.ToInt16(Console.ReadLine());
-
-                            Console.WriteLine("Digita el segundo número: ");
-                            num2 = Convert.ToInt16(Console.ReadLine());
-
-                            resultado = num1 * num2;
-
-                            Console.WriteLine("El resultado es: {0}", resultado);
-
-                            break;
-                    }
-
-                } while (opcion < 5);
-            }
-            catch (FormatException)
-            {
-                Console.WriteLine("El formato no es valido, solo es permitido números enteros");
-            }catch (OverflowException)
-            {
-                Console.WriteLine("Lo siento el número es demaciado grande, intentelo con otro mas pequeño");
+                Thread.Sleep(1000);
+                Console.WriteLine("{0}", valor);
             }
 
             Console.ReadKey();
