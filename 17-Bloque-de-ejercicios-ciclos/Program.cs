2bc921efc23caafa0533f84fcafdac7de1ab065c
Carlos Alberto Nin Queliz <128722564+Carlosnin0623@users.noreply.github.com>
2025-02-06 23:03:05 -0400

    Ejercicio con bucles para mejorar

---
+++ 17-Bloque-de-ejercicios-ciclos/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17_Bloque_de_ejercicios_ciclos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            byte num;
+            short suma = 0;
+
+            Console.WriteLine("Digita el número: ");
+            num = Convert.ToByte(Console.ReadLine());
+
+            for(byte i = 1; i < num; i++)
+            {
+               if(i % 2 == 0)
+                {
+                    Console.WriteLine("{0}", i);
+                    suma += i; 
+                }
+            }
+
+            Console.WriteLine("La suma total fue: {0}", suma);
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
