d67516fc110e6e09ee61b950d62c9afbc7d6718f
Carlos Alberto Nin Queliz <128722564+Carlosnin0623@users.noreply.github.com>
2025-02-07 22:38:38 -0400

    Ejercicios Ciclos

--- 17-Bloque-de-ejercicios-ciclos/Program.cs
+++ 17-Bloque-de-ejercicios-ciclos/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace _17_Bloque_de_ejercicios_ciclos
 {
@@ -12,23 +13,38 @@ namespace _17_Bloque_de_ejercicios_ciclos
         {
 
             byte num;
-            short suma = 0;
+            byte valor = 1;
+            byte capturar = 0;
 
-            Console.WriteLine("Digita el número: ");
-            num = Convert.ToByte(Console.ReadLine());
+            Stopwatch stopwatch = new Stopwatch();
 
-            for(byte i = 1; i < num; i++)
+            stopwatch.Start();
+
+            try
             {
-               if(i % 2 == 0)
+                Console.WriteLine("Introduzca el número: ");
+                num = Convert.ToByte(Console.ReadLine());
+
+                while (valor <= num)
                 {
-                    Console.WriteLine("{0}", i);
-                    suma += i; 
+                    Console.WriteLine("{0}", valor);
+                    capturar += valor;
+                    valor++;
                 }
+
+                Console.WriteLine("La suma de números es: {0} y el número limite: {1}", capturar, num);
+
+            }catch (Exception error)
+            {
+                Console.WriteLine($"Error: {error.Message}");
             }
 
-            Console.WriteLine("La suma total fue: {0}", suma);
 
+            // Detener el cronómetro
+            stopwatch.Stop();
 
+            // Mostrar el tiempo transcurrido
+            Console.WriteLine("El código tardó: " + stopwatch.ElapsedMilliseconds + " milisegundos");
 
             Console.ReadKey();
 
