4cea8b7730e31b650a99198eba3c843a268ca25e
Carlos Alberto Nin Queliz <128722564+Carlosnin0623@users.noreply.github.com>
2025-02-09 11:52:41 -0400

    Metodor-parte2

---
+++ 20-Metodos-parte2/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20_Metodos_parte2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte opcion;
+            float resultado;
+
+            Console.WriteLine("Escoge una de estas opciones ");
+            Console.WriteLine("1. Sumar");
+            Console.WriteLine("2. Restar");
+            Console.WriteLine("3. Multiplicar");
+            Console.WriteLine("4. Dividir");
+            Console.WriteLine("¿Que deseas hacer? ");
+
+            try
+            {
+                opcion = Convert.ToByte(Console.ReadLine());
+
+
+                if (opcion == 1)
+                {
+                    Sumar();
+
+                }
+                else if (opcion == 2)
+                {
+                    resultado = Restar();
+                    Console.WriteLine("El resultado de la resta es: {0}", resultado);
+                }
+                else if (opcion == 3)
+                {
+                    
+                }
+                else if (opcion == 4)
+                {
+                    
+                }
+                else
+                {
+                    Console.WriteLine("La opción elegida no es valida....");
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Lo siento no se permiten valores alphanuméricos");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Lo siento el número ingresado es muy pequeño o muy grande, intente nuevamente");
+
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("No puedes dividir entre 0");
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine("Ha ocurrido un error: " + error.Message + " " + " debe agregar la siguiente expecpción: " + error.GetType());
+            }
+
+            Console.ReadKey();
+        }
+
+
+        static void Sumar()
+        {
+            float numero1;
+            float numero2;
+            float resultado;
+
+            Console.WriteLine("Digite el primer númeero");
+            numero1 = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine("Digite el segundo númeero");
+            numero2 = Convert.ToSingle(Console.ReadLine());
+
+            resultado = numero1 + numero2;
+
+            Console.WriteLine("El resultado es: {0}", resultado);
+        }
+
+        static float Restar()
+        {
+            float numero1;
+            float numero2;
+            float resultado;
+
+            Console.WriteLine("Digite el primer númeero");
+            numero1 = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine("Digite el segundo númeero");
+            numero2 = Convert.ToSingle(Console.ReadLine());
+
+            resultado = numero1 - numero2;
+
+            return resultado;
+        }
+    }
+}
