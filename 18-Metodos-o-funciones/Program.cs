d62c30df7a28b91de2910cdcc51c8cbe44c0b5b4
Carlos Alberto Nin Queliz <128722564+Carlosnin0623@users.noreply.github.com>
2025-02-08 23:27:20 -0400

    Pracitcando los metodos

---
+++ 18-Metodos-o-funciones/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18_Metodos_o_funciones
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /* 
+               Metodos y funciones
+
+               La función o metodo es un elemento del programa que contiene codigo
+               y se puede ejecutar, es decir, lleva a cabo una operacion o conjunto
+               de instrucciones. La funcion o metodo puede llamarse o invocarse cuando
+               sea necesario y entonces el codigo que se encuentra en su interior se 
+               va a ejecutar. Una vez que la función termina de ejecutarse el programa
+               continua en la sentencia siguiente donde fue llamada.
+             
+             */
+
+            Console.WriteLine("Digite el primer número: ");
+            int numero1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite el segundo número: ");
+            int numero2 = Convert.ToInt32(Console.ReadLine());
+
+            int resultado = sumar(numero1, numero2);
+
+            Console.WriteLine("El resultado de la suma es: {0}", resultado);
+
+            Console.ReadKey();
+
+
+        }
+
+
+        static int sumar(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+
+
+    }
+}
