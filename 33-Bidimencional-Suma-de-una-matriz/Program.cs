39a5c4e1f7d03d6689e3a031e902a324235fbee7
Carlos Alberto Nin Queliz <carlosnin648@gmail.com>
2025-02-26 15:19:00 -0400

    Creando un arreglo de 2 dimenciones

---
+++ 33-Bidimencional-Suma-de-una-matriz/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _33_Bidimencional_Suma_de_una_matriz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 
+             *   Crea un programa que calcule la suma de de los números de una matriz 
+             *   predefinidos e imprima el resultado en pantalla.
+             */
+
+
+            int[,] matriz =
+            {
+                {1,2,3},
+                {4,5,6},
+                {7,8,9}
+             };
+
+
+
+            for (int i = 0; i < matriz.GetLength(0); i++)
+            {
+                for (int j = 0; j < matriz.GetLength(1); j++)
+                {
+                    Console.WriteLine(matriz[i, j]);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
